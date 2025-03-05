27f8813cc25c52fcf908292175c172cb9b9672dc
Rahul <146613451+Neural-Net-Rahul@users.noreply.github.com>
2025-03-05 20:19:54 +0530

    Update Signup.tsx

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -64,7 +64,7 @@ const Signup = () => {
       }
       try{
         setLoading(true);
-        const response = await axios.post('http://localhost:3000/api/users/register',formData,  {
+        const response = await axios.post('https://tracks-2qce.onrender.com/api/users/register',formData,  {
           headers: { "Content-Type": "multipart/form-data" }
         });
         if(response.status === 200){
