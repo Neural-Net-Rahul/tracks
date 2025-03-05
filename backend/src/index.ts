5122578269cc55d9bea9c0042a2144efe70416de
Rahul <146613451+Neural-Net-Rahul@users.noreply.github.com>
2025-03-05 22:33:23 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,14 +4,6 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-// Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Catch-all route to handle client-side routing
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 const PORT_NO = process.env.PORT_NO || 3001;
 
 app.listen(PORT_NO,()=>{
